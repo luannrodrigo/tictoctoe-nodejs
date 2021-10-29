fc6dcca78ec3762477a7d4285fa72341220b9fc4
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-10-29 17:53:07 -0300

    refact validations

--- src/app/services/BoardService.js
+++ src/app/services/BoardService.js
@@ -1,8 +1,21 @@
-import Board from '../methods/Boad';
+import AppError from '../../errors/AppError';
+import Board from '../methods/Board';
 
 class BoardService {
-  async run({ splitedBoard }) {
-    const hash = new Board(splitedBoard);
+  async run({ splittedBoard }) {
+    if (splittedBoard.length === 0) {
+      throw new AppError('Board Cannot be null/undefined', 400);
+    }
+
+    const checkPlayers = splittedBoard.every(
+      (player) => player === 'x' || player === 'o' || player === ' '
+    );
+
+    if (!checkPlayers) {
+      throw new AppError('Invalid players', 400);
+    }
+
+    const hash = new Board(splittedBoard);
 
     const board = hash.getBoard(hash, 'o');
 
